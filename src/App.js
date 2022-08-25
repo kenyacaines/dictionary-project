f764e23db28f322f3ef05007170f33065ba6f1d4
kenyacaines <103071126+kenyacaines@users.noreply.github.com>
2022-08-25 14:41:32 -0500

    Added Search Engine Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import React from "react";
+import Dictionary from "./Dictionary";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <div className="container">
+        <header className="App-header text-center">
+          Kenya's Dictionary App
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Kenya Caines</small>
+        </footer>
+      </div>
     </div>
   );
 }
