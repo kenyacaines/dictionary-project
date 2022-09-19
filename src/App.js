5556b8368a4ff7f86110fbd0527ac43dbabaebc2
kenyacaines <103071126+kenyacaines@users.noreply.github.com>
2022-09-19 17:41:30 -0500

    Updated design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import React from "react";
 import Dictionary from "./Dictionary";
-import './App.css';
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
         <header className="App-header text-center">
-         Funky Dictionary
+          <h1 className="title-funky">Funky</h1>
+          <h1 className="title-dictionary">Dictionary</h1>
         </header>
         <main>
           <Dictionary defaultKeyword="sunset" />
         </main>
         <footer className="App-footer">
-          <small>Coded with love by Kenya Caines</small>
+          <small>
+            Coded with love by Kenya Caines,
+            <a
+              href="https://github.com/kenyacaines/dictionary-project"
+              rel="no referrer"
+            >
+              open-sourced.
+            </a>
+          </small>
         </footer>
       </div>
     </div>
   );
 }
-
-
